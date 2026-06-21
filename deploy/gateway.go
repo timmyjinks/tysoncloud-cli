e186cdd93f8d2b40a3d6c3c7293a5c62c4d30253
Tyson Jenkins <114427348+timmyjinks@users.noreply.github.com>
2026-06-20 18:12:46 -0600

    Update deploy/gateway.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- deploy/gateway.go
+++ deploy/gateway.go
@@ -31,7 +31,7 @@ func (d *DeployService) createHTTPRoute(deployment Deployment) error {
 				},
 			},
 			Hostnames: []gatewayv1.Hostname{
-				gatewayv1.Hostname(fmt.Sprintf("%s.tysoncloud.tysonjenkins.dev", deployment.Namespace)),
+				gatewayv1.Hostname(fmt.Sprintf("%s-%s.tysoncloud.tysonjenkins.dev", deployment.Namespace, deployment.Name)),
 			},
 			Rules: []v1.HTTPRouteRuleApplyConfiguration{
 				{
