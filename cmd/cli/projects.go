3a07fcf87ea30efa1fd9a7eeff1cb665ee7e3eb3
timmyjinks <tysonjjenkins.github@gmail.com>
2026-06-21 01:50:17 -0600

    fix: change arg count

--- cmd/cli/projects.go
+++ cmd/cli/projects.go
@@ -32,7 +32,7 @@ var deleteDeploymentCmd = &cobra.Command{
 	Annotations: map[string]string{
 		"deploy": "true",
 	},
-	Args: cobra.ExactArgs(1),
+	Args: cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		namespace := args[0]
 		err := app.dsvc.Delete(namespace)
